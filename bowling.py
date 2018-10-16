9a7cbefc27f8e7049f5d3d5f1a82a7ca1e35fa73
Kenny Deng <kennydeng352@gmail.com>
2018-10-16 18:44:21 -0400

    V2--Beginning of structure for strike detection

--- bowling.py
+++ bowling.py
@@ -1,5 +1,22 @@
-player input
-scan through input
-check for spare or strike 
-if spare or strike do math for it taking new input and skipping the rest of the other steps
-keep adding input to total
+# Program plan:
+# player input
+# scan through input
+# check for spare or strike 
+# if spare or strike do math for it taking new input and skipping the rest of the other steps
+# keep adding input to total
+player_input = input("Enter your bowling scores here: ")
+total_score = 0
+strike_counter = 1
+strike = False
+for score in player_input:
+	if strike == True:
+		total_score += score * 2
+		strike_counter += 1
+		if strike_counter == 3:
+			strike_counter = 1
+			strike = False
+	if score == 'X':
+		strike = True
+	else:
+		total_score += score	  
+print(score)
